a6af082de380111ac3b7837ff93a5fdef9be60e4
turi <73997195+sookm@users.noreply.github.com>
2021-09-08 13:28:54 +0900

    Update test.sol

--- test.sol
+++ test.sol
@@ -1,15 +1,26 @@
 pragma solidity ^0.4.11;
 
-contract SimpleStorage {
-    uint storedData;
-    uint storedDataValue;
+contract Example005 {  
+  function operation() returns (uint result) {  // function을 많이 지정함
+    result = sum(11,5);  
     
-    function set(uint x, uint y) {
-        storedData = x;
-        storedDataValue = y;
+  }
+
+  function sum(uint a, uint b) returns (uint) {
+      uint result = a + b;
+      return result;
+    }
+  function min(uint a, uint b) returns (uint) {
+      uint result = a - b;
+      return result;
+    }
+  function mul(uint a, uint b) returns (uint) {
+      uint result = a*b;
+      return result;
+    }
+  function div(uint a, uint b) returns (uint) {
+      uint result = a/b;
+      return result;
     }
 
-    function get() constant returns (uint, uint) {
-        return (storedData, storedDataValue);
-    }
 }
